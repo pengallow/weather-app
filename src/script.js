a5a834077c17b2a10f6d692fbcedb9be9ffa4403
pengallow <43455176+pengallow@users.noreply.github.com>
2021-02-24 19:51:21 -0500

    Update script.js
    "Current Location" button now updates the name of the city and the temperature based on user's GPS location.

--- src/script.js
+++ src/script.js
@@ -26,15 +26,25 @@ function searchCityNow(event) {
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
 
-    let apiKey = "3b69d9e884899e81040ee4e357f33f8b";
-    let apiLink = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric`;
+    let apiKeyLocation = "pk.42b261a2635a12bf1a7a6310a9d020be";
+    let apiUrlLocation = `https://us1.locationiq.com/v1/reverse.php?key=${apiKeyLocation}&lat=${latitude}&lon=${longitude}&format=json`;
+
+    function showCityNow(response) {
+      let currentLocationCity = response.data.address.county.replace(`City of`, ``);
+      let userCurrentCity = document.querySelector("#target-city");
+      userCurrentCity.innerHTML = `${currentLocationCity}`;
+    }
+    axios.get(`${apiUrlLocation}`).then(showCityNow);
+
+    let apiKeyWeather = "3b69d9e884899e81040ee4e357f33f8b";
+    let apiLinkWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric`;
 
     function showTemperature(response) {
       let currentTemp = Math.round(response.data.main.temp);
       let newTemp = document.querySelector("#search-temperature");
       newTemp.innerHTML = `${currentTemp}º`;
     }
-    axios.get(`${apiLink}&appid=${apiKey}`).then(showTemperature);
+    axios.get(`${apiLinkWeather}&appid=${apiKeyWeather}`).then(showTemperature);
   }
   navigator.geolocation.getCurrentPosition(handlePosition);
 }
