034117d0c8a571e765a4b1530e18813aa5d150ac
pengallow <43455176+pengallow@users.noreply.github.com>
2021-03-16 02:24:56 -0400

    Added class "text-center" to JS to center the forecast icons. Removed redundant CSS.

--- src/script.js
+++ src/script.js
@@ -69,7 +69,7 @@ function searchCity(event) {
 
       for (let index = 0; index < 6; index++) {
         forecast = response.data.list[index];
-        forecastElement.innerHTML += `<div class="col-2">
+        forecastElement.innerHTML += `<div class="col-2 text-center">
                             <h3>${formatHours(forecast.dt * 1000)}</h3>
                             <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png" />
                             <div class="weather-forecast-temperature">
@@ -122,7 +122,7 @@ function searchCityNow(event) {
 
         for (let index = 0; index < 6; index++) {
           forecast = response.data.list[index];
-          forecastElement.innerHTML += `<div class="col-2">
+          forecastElement.innerHTML += `<div class="col-2 text-center">
                             <h3>${formatHours(forecast.dt * 1000)}</h3>
                             <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png" />
                             <div class="weather-forecast-temperature">
